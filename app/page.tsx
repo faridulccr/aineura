5f47cd54fde261a65ec0b4a071a2b7414d999b14
Md Faridul islam <mdfaridulislamovi@gmail.com>
2024-03-26 22:36:04 +0600

    local font added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <main className="">
-            <h1 className=" font-bold">Hello Prismic</h1>
+            <h1 className="font-bold">Hello Prismic</h1>
         </main>
     );
 }
